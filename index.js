a1fdc98cce79f748b28486d61552a496aca4402e
Rejaur9131 <raju_seu@yahoo.com>
2022-05-21 21:58:18 +0600

    connection made with mongodb

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const cors = require('cors');
+require('dotenv').config();
+const jwt = require('jsonwebtoken');
+const port = process.env.PORT || 5000;
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.pjo3h.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+  try {
+    await client.connect();
+    console.log('DB connected');
+  } finally {
+  }
+}
+
+run().catch(console.dir());
+
+app.get('/', (req, res) => {
+  res.send('Welcome to my Manufacturing Site');
+});
+
+app.listen(port, () => {
+  console.log(`Manufacturing Site listening on port ${port}`);
+});
