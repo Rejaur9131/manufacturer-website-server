b6648cbe2d6d70b67fc168b3d1bf0c0da81c04fe
Rejaur9131 <raju_seu@yahoo.com>
2022-05-28 22:34:00 +0600

    fixed some errors

--- index.js
+++ index.js
@@ -7,7 +7,13 @@ const port = process.env.PORT || 5000;
 const app = express();
 
 // Middleware
-app.use(cors());
+const corsOptions = {
+  origin: 'http://localhost:3000',
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200
+};
+app.use(cors(corsOptions));
+
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.pjo3h.mongodb.net/?retryWrites=true&w=majority`;
